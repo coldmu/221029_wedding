ee3784ad3a4f428a2e0d7154e44a240901a34923
coldmu <coldmu@gmail.com>
2022-03-28 14:08:45 +0900

    init

---
+++ docs/js/about.ec11c6eb.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkwedding"]=self["webpackChunkwedding"]||[]).push([[443],{7574:function(n,e,u){u.r(e),u.d(e,{default:function(){return f}});var t=u(3396);const a={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function i(n,e){return(0,t.wg)(),(0,t.iD)("div",a,s)}var c=u(89);const d={},o=(0,c.Z)(d,[["render",i]]);var f=o}}]);
+//# sourceMappingURL=about.ec11c6eb.js.map
