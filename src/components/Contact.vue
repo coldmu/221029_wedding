e132e76705f7e4d29fd0b35904d41d8d14f32f0d
coldmu <coldmu@gmail.com>
2022-09-15 10:49:15 +0900

    IE copy

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -200,13 +200,13 @@ export default {
         document.body.appendChild(textarea);
 
         // 복사할 특정 텍스트를 임시의 textarea에 넣어주고 모두 셀렉션 상태
-        textarea.value = text;
+        textarea.value = param;
         textarea.select();
 
         document.execCommand('copy');
 
         // 복사 후 textarea 지우기
-        document.body.removeChild($textarea);
+        document.body.removeChild(textarea);
       }
     };
 
